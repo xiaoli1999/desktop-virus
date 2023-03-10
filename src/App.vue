37b3ea3610c4b46078267565235bfeff5e851968
小黎 <[22708206@qq.com]>
2023-03-10 13:46:43 +0800

    feat(all): 初次提交
    初次提交

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>测试</div>
+</template>
+
+<script lang="ts" setup>
+import { ref, onMounted, nextTick, onBeforeUnmount } from 'vue'
+
+/**
+ * @function judgePC 判断是当前浏览器信息是否为pc
+ * @return { Boolean } 返回是否是pc
+ */
+
+console.log('%c 整蛊桌面🌈 | 黎 | https://xiaoli1999.github.io/desktop-virus ', 'color: #f4f4f4;background: #444; padding:5px 0;border-radius:2px;')
+</script>
+
+<style lang="less">
+
+</style>
